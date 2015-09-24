e3b19dcdc9d653505cf2cd92337169b8ae4ded53
Tim van der Meij <timvandermeij@gmail.com>
2015-09-24 15:20:54 +0200

    Integrate viewer into the XBee sensor simulator
    Thanks to the component-based approach, this integration went fairly
    easily. One now has a dynamic viewer along with the printed results from
    the simulator. This helps to visualize the implemented scheduling
    algorithm.

--- xbee_sensor_simulator.py
+++ xbee_sensor_simulator.py
@@ -1,10 +1,54 @@
 import sys
 import time
 import json
-import argparse
 import socket
+import matplotlib.pyplot as plt
+import numpy as np
 from random import randint
 from settings.Settings import Settings
+from math import *
+
+class Viewer:
+    def __init__(self, settings):
+        # Initialize the viewer with a correctly scaled plot.
+        self.points = []
+        self.arrows = []
+        self.settings = settings
+
+        plt.xlim(0, self.settings.get("size"))
+        plt.ylim(0, self.settings.get("size"))
+        plt.gca().set_aspect("equal", adjustable="box")
+        plt.ion()
+        plt.show()
+
+    def draw_points(self):
+        # Draw a static point for each sensor in a circular shape. The
+        # spacing between the points is equal. We add an offset to display
+        # the circle in the middle of the plot.
+        offset = self.settings.get("size") / 2
+        for angle in np.arange(0, 2 * pi, (2 * pi) / self.settings.get("number_of_sensors")):
+            x = offset + (cos(angle) * self.settings.get("circle_radius"))
+            y = offset + (sin(angle) * self.settings.get("circle_radius"))
+            self.points.append((x, y))
+            plt.plot(x, y, linestyle="None", marker="o", color="black", markersize=10)
+
+    def draw_arrow(self, point_from, point_to):
+        # Draw an arrow from a given point to another given point.
+        options = {
+            "arrowstyle": "<-, head_width=1, head_length=1",
+            "color": "red",
+            "linewidth": 2
+        }
+        arrow = plt.annotate("", self.points[point_from - 1], self.points[point_to - 1], arrowprops=options)
+        self.arrows.append(arrow)
+        plt.draw()
+
+    def clear_arrows(self):
+        # Remove all arrows from the plot.
+        for arrow in self.arrows:
+            arrow.remove()
+
+        self.arrows = []
 
 class TDMA_Scheduler(object):
     def __init__(self, settings, id):
@@ -40,31 +84,29 @@ class TDMA_Scheduler(object):
         return self.timestamp
 
 class XBee_Sensor(object):
-    def __init__(self, settings, id):
+    def __init__(self, id, settings, viewer):
         # Initialize the sensor with its ID and a unique, non-blocking UDP socket.
-        self.settings = settings
         self.id = id
+        self.settings = settings
+        self.viewer = viewer
         self.next_timestamp = 0
         self.scheduler = TDMA_Scheduler(self.settings, self.id)
+        self.next_timestamp = self.scheduler.get_next_timestamp()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.settings.get("ip"), self.settings.get("port") + self.id))
         self.socket.setblocking(0)
 
-    def run(self):
-        # Execute the sensor's main loop that constantly sends and receives packets.
-        self.next_timestamp = self.scheduler.get_next_timestamp()
-        while True:
-            # Add a small delay to avoid 100% CPU usage due to the while loop.
-            time.sleep(self.settings.get("loop_delay"))
+    def activate(self):
+        # Activate the sensor to send and receive packets.
+        if time.time() >= self.next_timestamp:
+            self._send()
+            self.next_timestamp = self.scheduler.get_next_timestamp()
 
-            if time.time() >= self.next_timestamp:
-                self._send()
-                self.next_timestamp = self.scheduler.get_next_timestamp()
-
-            self._receive()
+        self._receive()
 
     def _send(self):
         # Send packets to all other sensors.
+        self.viewer.clear_arrows()
         for i in range(1, self.settings.get("number_of_sensors") + 1):
             if i == self.id:
                 continue
@@ -76,6 +118,7 @@ class XBee_Sensor(object):
                 "rssi": -randint(1,60)
             }
             self.socket.sendto(json.dumps(packet), (self.settings.get("ip"), self.settings.get("port") + i))
+            self.viewer.draw_arrow(self.id, i)
             print("-> {} sending at {}...".format(self.id, packet["timestamp"]))
 
     def _receive(self):
@@ -89,16 +132,20 @@ class XBee_Sensor(object):
 
 def main(argv):
     settings = Settings("settings.json", "xbee_sensor_simulator")
-    
-    parser = argparse.ArgumentParser(description="Simulate packet communication in a WiFi sensor network.")
-    parser.add_argument("-i", "--id", default=0, dest="id", type=int, help="Unique identifier for the sensor.")
 
-    args = parser.parse_args()
-    if args.id < 1 or args.id > settings.get("number_of_sensors"):
-        print("Provide a non-negative and non-zero ID that is smaller or equal to the total number of sensors")
-    else:
-        sensor = XBee_Sensor(settings, args.id)
-        sensor.run()
+    viewer = Viewer(settings)
+    viewer.draw_points()
+
+    sensors = []
+    for sensor_id in range(1, settings.get("number_of_sensors") + 1):
+        sensor = XBee_Sensor(sensor_id, settings, viewer)
+        sensors.append(sensor)
+
+    while True:
+        for sensor in sensors:
+            sensor.activate()
+        
+        time.sleep(settings.get("loop_delay"))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
