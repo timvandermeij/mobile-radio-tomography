fe09fa81d7faece5081834d78782019844691208
Tim van der Meij <timvandermeij@gmail.com>
2015-09-24 14:03:16 +0200

    Make the viewer refresh itself

--- viewer.py
+++ viewer.py
@@ -1,17 +1,22 @@
+import time
 import matplotlib.pyplot as plt
 import numpy as np
 from settings.Settings import Settings
 from math import *
+from random import randint
 
 class Viewer:
     def __init__(self, settings):
         # Initialize the viewer with a correctly scaled plot.
         self.points = []
+        self.arrows = []
         self.settings = settings
 
         plt.xlim(0, self.settings.get("size"))
         plt.ylim(0, self.settings.get("size"))
         plt.gca().set_aspect("equal", adjustable="box")
+        plt.ion()
+        plt.show()
 
     def draw_points(self):
         # Draw a static point for each sensor in a circular shape. The
@@ -29,12 +34,25 @@ class Viewer:
         options = {
             "arrowstyle": "<-, head_width=1, head_length=1",
             "color": "red",
-            "linewidth": 3
+            "linewidth": 2
         }
-        plt.annotate("", self.points[point_from - 1], self.points[point_to - 1], arrowprops=options)
+        self.arrows.append(plt.annotate("", self.points[point_from - 1], self.points[point_to - 1], arrowprops=options))
 
     def display(self):
-        plt.show()
+        while True:
+            # Remove arrows from the previous image.
+            for arrow in self.arrows:
+                arrow.remove()
+
+            self.arrows = []
+
+            # Draw the new arrows.
+            for sensor_id in range(1, self.settings.get("number_of_sensors") - 1):
+                self.draw_arrow(sensor_id, randint(1, self.settings.get("number_of_sensors")))
+
+            plt.draw()
+            
+            time.sleep(self.settings.get("refresh_delay"))
 
 def main():
     settings = Settings("settings.json", "viewer")
