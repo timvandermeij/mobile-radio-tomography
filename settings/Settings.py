c6f0adc397ed897231964b44c99debad5741f0ae
Leon Helwerda <leon.helwerda@planet.nl>
2015-09-26 17:14:46 +0200

    Settings: Keep loaded JSON object in class
    Before now, each instantiation of Settings will open the JSON file,
    parse it and take one of the components from it. In the case that
    a script has multiple users of Settings, such as mission_basic.py with
    the 'distance_sensor_simulator' and the 'mission' itself, this means
    that the file is being opened multiple times. Later on, we might in fact
    use the XBee component from the trajectory mission as well.
    
    Therefore, keep the loaded object in a static variable and add a class
    method to load it. Since the Settings object can be given a different
    JSON file each time, use a dictionary so that they don't collide with
    each other.

--- settings/Settings.py
+++ settings/Settings.py
@@ -1,11 +1,26 @@
 import json
 
 class Settings(object):
+    settings_files = {}
+
+    @classmethod
+    def get_settings(cls, file_name):
+        """
+        Retrieve the settings from a file name, load the JSON data and 
+        unserialize it. We store the settings object statically in this class 
+        so that later uses of the same settings file can reuse it.
+        """
+
+        if file_name not in cls.settings_files:
+            with open(file_name) as data:
+                cls.settings_files[file_name] = json.load(data)
+
+        return cls.settings_files[file_name]
+
     def __init__(self, file_name, component_name):
         self.component_name = component_name
         
-        with open(file_name) as data:
-            settings = json.load(data)
+        settings = self.__class__.get_settings(file_name)
         
         self.settings = None
         if self.component_name in settings:
