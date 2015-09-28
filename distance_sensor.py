54c93b6c1fcdb575148233fab4e732fda2f196f1
Tim van der Meij <timvandermeij@gmail.com>
2015-09-28 12:33:33 +0200

    Import settings for the distance sensor

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,9 +1,11 @@
 import time
 from __init__ import __package__
+from settings import Settings
 from distance.Distance_Sensor_Physical import Distance_Sensor_Physical
 
 def main():
     distance_sensor = Distance_Sensor_Physical()
+    settings = Settings("settings.json", "distance_sensor_physical")
 
     while True:
         print("Measured distance: {} m".format(distance_sensor.get_distance()))
