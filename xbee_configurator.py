c22a5cf0657f37b01529976d8e679053e4b0c308
Tim van der Meij <timvandermeij@gmail.com>
2015-10-05 22:05:01 +0200

    Make the XBee configurator work for all sensors and use settings
    Reads the number of sensors `n` from the settings file and lets the user
    configure `n + 1` sensors as the ground station also needs to be
    configured and is left apart from the total sensor count.
    
    Additionally we make use of the settings component to store and fetch
    the settings for this component.

--- xbee_configurator.py
+++ xbee_configurator.py
@@ -1,14 +1,22 @@
+from settings import Settings
 from zigbee.XBee_Configurator import XBee_Configurator
 
 def main():
-    id = 4
-    xbee_configurator = XBee_Configurator(id, "/dev/ttyUSB1", 9600)
-    xbee_configurator.set("ID", 5678)
-    xbee_configurator.set("NI", str(id))
-    xbee_configurator.write()
-    xbee_configurator.get("ID")
-    xbee_configurator.get("NI")
-    del xbee_configurator
+    settings = Settings("settings.json", "xbee_configurator")
+
+    for id in range(0, settings.get("number_of_sensors") + 1):
+        if id == 0:
+            raw_input("Connect the ground station XBee sensor and press Enter.")
+        else:
+            raw_input("Connect XBee sensor {} and press Enter...".format(id))
+
+        xbee_configurator = XBee_Configurator(id, settings.get("source"), settings.get("baud_rate"))
+        xbee_configurator.set("ID", settings.get("pan_id"))
+        xbee_configurator.set("NI", str(id))
+        xbee_configurator.write()
+        xbee_configurator.get("ID")
+        xbee_configurator.get("NI")
+        del xbee_configurator
 
 if __name__ == "__main__":
     main()
