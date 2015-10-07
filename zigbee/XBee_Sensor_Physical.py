1ef72fd33752120b695a5f0c230952684824b19c
Tim van der Meij <timvandermeij@gmail.com>
2015-10-07 22:49:51 +0200

    Decrease loop delay and add a TODO

--- zigbee/XBee_Sensor_Physical.py
+++ zigbee/XBee_Sensor_Physical.py
@@ -2,6 +2,7 @@
 # TODO: Document that exactly one sensor has to be a coordinator.
 # TODO: Implement the stubbed methods, partly by moving methods below.
 # TODO: Implement network discovery to remove the hardcoded sensors array.
+# TODO: Fix the start-up delay such that sensors start transmitting immediately.
 
 from xbee import ZigBee
 import serial
