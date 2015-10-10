89faf88f8a621290e433be7a9d4e3f5e618456ae
Tim van der Meij <timvandermeij@gmail.com>
2015-10-10 17:07:58 +0200

    Implement sensor address handling
    This patch moves the sensor addresses to the settings file.
    Additionally, it implement sensor address handling, i.e., setting the
    current sensor's address and sending to all other addresses in the
    network except for its own address. Furthermore the receive handler is
    solidified to check which type of message is received.
    
    Note that we do not perform a node discovery for the sensors. Node
    discovery works by sending a broadcast message, which is very bad for
    performance. On top of that, there is no receive acknowledgement, so we
    do not know if all nodes are actually aware of the existence. Finally,
        since we have to specify the number of nodes in the network anyway,
        specifying their addresses is not unrealistic.

--- zigbee/XBee_Sensor_Physical.py
+++ zigbee/XBee_Sensor_Physical.py
@@ -1,4 +1,3 @@
-# TODO: Implement network discovery to remove the hardcoded sensors array.
 # TODO: Fix the start-up delay such that sensors start transmitting immediately.
 # TODO: Unit testing.
 # TODO: RSSI list and transmission to ground station.
@@ -12,10 +11,6 @@ from ..settings import Arguments, Settings
 
 class XBee_Sensor_Physical(XBee_Sensor):
     STATUS_OK = "\x00"
-    SENSORS = [
-        "\x00\x13\xa2\x00@\xe6n\xb9", # Address of sensor 1
-        "\x00\x13\xa2\x00@\xe6o5" # Address of sensor 2
-    ]
 
     def __init__(self, sensor_id, settings, scheduler):
         """
@@ -34,6 +29,7 @@ class XBee_Sensor_Physical(XBee_Sensor):
         self.next_timestamp = self.scheduler.get_next_timestamp()
         self._serial_connection = None
         self._sensor = None
+        self._address = None
 
     def activate(self):
         """
@@ -46,6 +42,10 @@ class XBee_Sensor_Physical(XBee_Sensor):
             self._serial_connection = serial.Serial("/dev/ttyUSB{}".format(self.id - 1),
                                                     self.settings.get("baud_rate"))
             self._sensor = ZigBee(self._serial_connection, callback=self._receive)
+
+            # Set this sensor's address.
+            self._sensor.send("at", command="SH")
+            self._sensor.send("at", command="SL")
 
         if self.id > 0 and time.time() >= self.next_timestamp:
             self._send()
@@ -61,15 +61,21 @@ class XBee_Sensor_Physical(XBee_Sensor):
 
     def _send(self):
         """
-        Send a packet to a sensor in the network.
+        Send a packet to each other sensor in the network.
         """
 
         packet = {
             "from": self.id,
             "timestamp": time.time()
         }
-        self._sensor.send("tx", dest_addr_long=self.SENSORS[self.id % 2], dest_addr="\xFF\xFE",
-                          frame_id="\x01", data=json.dumps(packet))
+        for sensor_address in self.settings.get("sensors"):
+            # Unescape the string as it is escaped in JSON.
+            sensor_address = sensor_address.decode("string_escape")
+
+            if sensor_address != self._address:
+                self._sensor.send("tx", dest_addr_long=sensor_address,
+                                  dest_addr="\xFF\xFE", frame_id="\x01",
+                                  data=json.dumps(packet))
 
     def _receive(self, packet):
         """
@@ -77,7 +83,7 @@ class XBee_Sensor_Physical(XBee_Sensor):
         """
 
         if self.id > 0:
-            if "rf_data" in packet:
+            if packet["id"] == "rx":
                 payload = json.loads(packet["rf_data"])
 
                 print("Sensor {} received a packet from sensor {}.".format(self.id, payload["from"]))
@@ -87,6 +93,20 @@ class XBee_Sensor_Physical(XBee_Sensor):
 
                 # Request the RSSI value for the received packet.
                 self._sensor.send("at", command="DB")
-            elif "parameter" in packet:
-                rssi = ord(packet["parameter"])
-                print("Sensor {} received the packet with RSSI -{} dBm.".format(self.id, rssi))
+            elif packet["id"] == "at_response":
+                if packet["command"] == "DB":
+                    # RSSI value has been received.
+                    rssi = ord(packet["parameter"])
+                    print("Sensor {} received the packet with RSSI -{} dBm.".format(self.id, rssi))
+                elif packet["command"] == "SH":
+                    # Serial number (high) has been received.
+                    if self._address == None:
+                        self._address = packet["parameter"]
+                    else:
+                        self._address = packet["parameter"] + self._address
+                elif packet["command"] == "SL":
+                    # Serial number (low) has been received.
+                    if self._address == None:
+                        self._address = packet["parameter"]
+                    else:
+                        self._address = self._address + packet["parameter"]
