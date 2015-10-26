ec0aaecf4cafaa4e5545b754c9156fed4add0579
Tim van der Meij <timvandermeij@gmail.com>
2015-10-26 23:22:03 +0100

    Implement XBee packet class
    For flexibility this is a simple key-value store with methods for
    serializing to and unserializing from JSON strings. These JSON strings
    are used in the payload of the sent packets.

---
+++ zigbee/XBee_Packet.py
@@ -0,0 +1,40 @@
+import json
+
+class XBee_Packet(object):
+    def __init__(self):
+        """
+        Initialize the packet with an empty contents key-value store.
+        """
+
+        self._contents = {}
+
+    def set(self, key, value):
+        """
+        Set a key and value in the contents key-value store.
+        """
+
+        self._contents[key] = value
+
+    def get(self, key):
+        """
+        Get the value of a key in the contents key-value store.
+        """
+
+        if key in self._contents:
+            return self._contents[key]
+
+        return None
+
+    def serialize(self):
+        """
+        Convert the contents object to a JSON string.
+        """
+
+        return json.dumps(self._contents)
+
+    def unserialize(self, contents):
+        """
+        Convert the JSON string to a contents object.
+        """
+
+        self._contents = json.loads(contents)
