27c01900114cf2e9b35f3de50ec3a4be97ef0e70
Tim van der Meij <timvandermeij@gmail.com>
2015-11-10 12:39:13 +0100

    Merge the XBee_Custom_Packet and XBee_Packet unit tests

--- tests/xbee_packet.py
+++ tests/xbee_packet.py
@@ -1,4 +1,4 @@
-import json
+import struct
 import unittest
 from ..zigbee.XBee_Packet import XBee_Packet
 
@@ -9,6 +9,9 @@ class TestXBeePacket(unittest.TestCase):
     def test_initialization(self):
         # The packet must be empty.
         self.assertEqual(self.packet._contents, {})
+
+        # The specifications dictionary must be set.
+        self.assertIsInstance(self.packet._specifications, dict)
 
     def test_set(self):
         # A given key and value should be present in the contents.
@@ -43,19 +46,42 @@ class TestXBeePacket(unittest.TestCase):
         })
 
     def test_serialize(self):
-        # A JSON string of the contents dictionary should be returned.
-        self.packet._contents["foo"] = "bar"
-        serialized = self.packet.serialize()
-        dictionary = {
-            "foo": "bar"
-        }
-        self.assertEqual(serialized, json.dumps(dictionary))
+        # A specification must be provided.
+        with self.assertRaises(KeyError):
+            self.packet.serialize()
+
+        # A provided specification must exist.
+        self.packet.set("specification", "foo")
+        with self.assertRaises(KeyError):
+            self.packet.serialize()
+
+        # All fields from the specification must be provided.
+        self.packet.set("specification", "memory_map_chunk")
+        with self.assertRaises(KeyError):
+            self.packet.serialize()
+
+        # When all fields are provided, the specification field must be
+        # unset and the packed message must be valid.
+        self.packet.set("specification", "memory_map_chunk")
+        self.packet.set("latitude", 123456789.12)
+        self.packet.set("longitude", 123496785.34)
+        packed_message = self.packet.serialize()
+        self.assertEqual(self.packet.get("specification"), None)
+        self.assertEqual(packed_message, "\x01H\xe1zT4o\x9dA\xf6(\\E\xa5q\x9dA")
 
     def test_unserialize(self):
-        # The contents dictionary should be filled with the keys
-        # and values from the JSON string.
-        dictionary = {
-            "foo": "bar"
-        }
-        self.packet.unserialize(json.dumps(dictionary))
-        self.assertEqual(self.packet._contents, dictionary)
+        # Empty strings must be refused.
+        with self.assertRaises(struct.error):
+            self.packet.unserialize("")
+
+        # Invalid specifications must be refused.
+        with self.assertRaises(KeyError):
+            self.packet.unserialize("\xFF\x01")
+
+        # Valid messages must be unpacked.
+        self.packet.unserialize("\x01H\xe1zT4o\x9dA\xf6(\\E\xa5q\x9dA")
+        self.assertEqual(self.packet._contents, {
+            "specification": "memory_map_chunk",
+            "latitude": 123456789.12,
+            "longitude": 123496785.34
+        })
