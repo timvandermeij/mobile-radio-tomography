d563240e17a4ecde8df3ecf1b32b47d7d9151a27
Tim van der Meij <timvandermeij@gmail.com>
2016-02-22 16:46:21 +0100

    Implement unit tests for the line follower component
    - Add bit masks for lines on the left or on the right. This is necessary
      because the controller would like to have this information to correct
      the path to the nearest line.
    - Make the callback more general such that we can pass both intersection
      messages as well as messages when the rover diverged from the grid.
    - Fix a bug where the direction would not be recognized. It is an
      integer value, so we cannot use `isinstance` with
      `Line_Follower_Direction`.
    - Extract reading the sensors values into its own method, not only to
      make the `update` method smaller but also for the unit tests to be
      able to test it and the `update` method properly.
    - Fix a bug where we would try to mutate a tuple, which is not allowed
      in Python.
    - Implement the case for a diverging rover.

--- location/Line_Follower.py
+++ location/Line_Follower.py
@@ -12,11 +12,13 @@ class Line_Follower_State(object):
     AT_INTERSECTION = 2
 
 class Line_Follower_Bit_Mask(object):
-    LINE = 0xb0110
-    INTERSECTION = 0xb1001
+    LINE = 0b0110
+    LINE_LEFT = 0b1000
+    LINE_RIGHT = 0b0001
+    INTERSECTION = 0b1001
 
 class Line_Follower(object):
-    def __init__(self, location, direction, intersection_callback, settings):
+    def __init__(self, location, direction, callback, settings):
         """
         Initialize the line follower object. We assume that we are working
         with the Zumo Robot for Arduino v1.2 (assembled with 75:1 HP motors),
@@ -35,12 +37,12 @@ class Line_Follower(object):
         if not isinstance(location, tuple):
             raise ValueError("Location must be a tuple")
 
-        if not isinstance(direction, Line_Follower_Direction):
+        if type(direction) != int or not 1 <= direction <= 4:
             raise ValueError("Direction must be one of the defined types")
 
         self._location = location
         self._direction = direction
-        self._intersection_callback = intersection_callback
+        self._callback = callback
         self._state = Line_Follower_State.AT_INTERSECTION
 
         self._sensors = settings.get("sensors")
@@ -62,54 +64,76 @@ class Line_Follower(object):
         for sensor in self._sensors:
             self.gpio.setup(sensor, self.gpio.IN)
 
-    def update(self):
+    def read(self):
         """
-        Update the state and location of the vehicle by reading and interpreting
-        the values from the line follower.
+        Read the values of four of the six LEDs. We only read the two innermost
+        and the two outermost LEDs to clearly make a distinction between a
+        straight line and an intersection of lines.
         """
 
-        # Read the state of four of the six LEDs. We only read the two innermost
-        # and the two outermost LEDs to clearly make a distinction between a
-        # straight line and an intersection of lines.
-        sensor_0 = self.gpio.input(self._sensors[0])
-        sensor_2 = self.gpio.input(self._sensors[2])
-        sensor_3 = self.gpio.input(self._sensors[3])
-        sensor_5 = self.gpio.input(self._sensors[5])
+        sensor_values = []
+        for sensor in [0, 2, 3, 5]:
+            sensor_values.append(self.gpio.input(self._sensors[sensor]))
+
+        return sensor_values
+
+    def update(self, sensor_values):
+        """
+        Update the state and location of the vehicle by reading and interpreting
+        the sensor values from the line follower.
+        """
+
+        if type(sensor_values) != list or len(sensor_values) != 4:
+            raise ValueError("Sensor values must be a list with four elements")
 
         # Represent the state as an integer to allow for bit manipulations.
         state = 0
-        for sensor in [sensor_0, sensor_2, sensor_3, sensor_5]:
+        for sensor_value in sensor_values:
             state = state << 1
-            state += sensor
+            state += sensor_value
 
-        # Check if we have a line on the left or on the right, i.e., if we are
-        # at an intersection.
+        # Check if we are at a line or at an intersection and update the
+        # state and location of the vehicle accordingly.
         line = state & Line_Follower_Bit_Mask.LINE
         intersection = state & Line_Follower_Bit_Mask.INTERSECTION
         if line and not intersection:
             self._state = Line_Follower_State.AT_LINE
-        else:
+        elif line and intersection:
             self._state = Line_Follower_State.AT_INTERSECTION
-            
+
             # Update the location using the direction.
             if self._direction == Line_Follower_Direction.UP:
-                self._location[1] += 1
+                self._location = (self._location[0], self._location[1] + 1)
             elif self._direction == Line_Follower_Direction.DOWN:
-                self._location[1] -= 1
+                self._location = (self._location[0], self._location[1] - 1)
             elif self._direction == Line_Follower_Direction.LEFT:
-                self._location[0] -= 1
-            else:
-                self._location[0] += 1
+                self._location = (self._location[0] - 1, self._location[1])
+            elif self._direction == Line_Follower_Direction.RIGHT:
+                self._location = (self._location[0] + 1, self._location[1])
 
             # Notify the listener (callback) and pass the new location.
-            self._intersection_callback(self_location)
+            self._callback("intersection", self._location)
+        elif not line and intersection:
+            # The rover has diverged from the grid. Determine where the
+            # line on the grid is (left or right) so the rover controller
+            # can correct its path. If we detect a line on the left and
+            # on the right, we do nothing as we do not know where to
+            # correct the path to. The same holds for the situation where
+            # we do not detect any lines, which is why the else clause
+            # is missing below.
+            if intersection == Line_Follower_Bit_Mask.INTERSECTION:
+                return
+
+            is_line_left = (intersection == Line_Follower_Bit_Mask.LINE_LEFT)
+            is_line_right = (intersection == Line_Follower_Bit_Mask.LINE_RIGHT)
+            self._callback("diverged", "left" if is_line_left else "right")
 
     def set_direction(self, direction):
         """
         Set the direction of the vehicle.
         """
 
-        if not isinstance(direction, Line_Follower_Direction):
+        if type(direction) != int or not 1 <= direction <= 4:
             raise ValueError("Direction must be one of the defined types")
 
         self._direction = direction
---
+++ tests/location_line_follower.py
@@ -0,0 +1,207 @@
+import unittest
+from mock import patch, call, MagicMock
+from ..settings import Arguments
+
+class TestLocationLineFollower(unittest.TestCase):
+    def setUp(self):
+        # We need to mock the RPi.GPIO module as it is only available
+        # on Raspberry Pi devices and these tests run on a PC.
+        self.rpi_gpio_mock = MagicMock()
+        modules = {
+            'RPi': self.rpi_gpio_mock,
+            'RPi.GPIO': self.rpi_gpio_mock.GPIO
+        }
+
+        self.patcher = patch.dict('sys.modules', modules)
+        self.patcher.start()
+        from ..location.Line_Follower import Line_Follower_Direction
+
+        self.location = (0, 0)
+        self.direction = Line_Follower_Direction.UP
+        
+        arguments = Arguments("settings.json", [])
+        self.settings = arguments.get_settings("line_follower")
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def test_initialization(self):
+        from ..location.Line_Follower import Line_Follower, Line_Follower_State
+
+        mock_callback = MagicMock()
+
+        # Location must be a tuple.
+        with self.assertRaises(ValueError):
+            line_follower = Line_Follower([0, 0], self.direction, mock_callback, self.settings)
+
+        # Direction must be one of the defined types.
+        with self.assertRaises(ValueError):
+            line_follower = Line_Follower(self.location, "up", mock_callback, self.settings)
+
+        # Correct intialization should set the attributes.
+        line_follower = Line_Follower(self.location, self.direction, mock_callback, self.settings)
+        self.assertEqual(line_follower._location, self.location)
+        self.assertEqual(line_follower._direction, self.direction)
+        self.assertEqual(line_follower._callback, mock_callback)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+
+        # Warnings must be disabled.
+        line_follower.gpio.setwarnings.assert_called_once_with(False)
+
+        # Board numbering has to be used.
+        line_follower.gpio.setmode.assert_called_once_with(line_follower.gpio.BOARD)
+
+        # The input pins have to be set.
+        sensors = self.settings.get("sensors")
+        line_follower.gpio.setup.assert_has_calls([
+            call(sensor, line_follower.gpio.IN) for sensor in sensors
+        ])
+
+    def test_read(self):
+        from ..location.Line_Follower import Line_Follower, Line_Follower_Direction
+
+        mock_callback = MagicMock()
+        line_follower = Line_Follower(self.location, self.direction, mock_callback, self.settings)
+        sensors = self.settings.get("sensors")
+        sensor_values = line_follower.read()
+        
+        self.assertIs(type(sensor_values), list)
+        line_follower.gpio.input.assert_has_calls([
+            call(sensor) for sensor in [sensors[0], sensors[2], sensors[3], sensors[5]]
+        ])
+
+    def test_update(self):
+        from ..location.Line_Follower import Line_Follower, Line_Follower_Direction, Line_Follower_State
+
+        mock_callback = MagicMock()
+        line_follower = Line_Follower(self.location, self.direction, mock_callback, self.settings)
+
+        # Invalid sensor values should cause an error.
+        with self.assertRaises(ValueError):
+            line_follower.update(0b0110)
+
+        # It should detect being on a single line.
+        line_follower.update([0, 1, 1, 0])
+        self.assertEqual(line_follower._location, self.location)
+        self.assertEqual(line_follower._direction, self.direction)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_LINE)
+        self.assertFalse(mock_callback.called)
+        mock_callback.reset_mock()
+
+        # It should detect being on an intersection (both left and right lines).
+        line_follower.update([1, 1, 1, 1])
+        new_location = (self.location[0], self.location[1] + 1)
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, self.direction)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        mock_callback.assert_has_calls([
+            call("intersection", new_location)
+        ])
+        mock_callback.reset_mock()
+
+        # It should detect being on an intersection (only left line).
+        line_follower.update([1, 1, 1, 0])
+        new_location = (self.location[0], self.location[1] + 2)
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, self.direction)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        mock_callback.assert_has_calls([
+            call("intersection", new_location)
+        ])
+        mock_callback.reset_mock()
+
+        # It should detect being on an intersection (only right line).
+        line_follower.update([0, 1, 1, 1])
+        new_location = (self.location[0], self.location[1] + 3)
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, self.direction)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        mock_callback.assert_has_calls([
+            call("intersection", new_location)
+        ])
+        mock_callback.reset_mock()
+
+        # It should handle changing directions.
+        line_follower.set_direction(Line_Follower_Direction.DOWN)
+        line_follower.update([0, 1, 1, 1])
+        new_location = (self.location[0], self.location[1] + 2)
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, Line_Follower_Direction.DOWN)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        mock_callback.assert_has_calls([
+            call("intersection", new_location)
+        ])
+        mock_callback.reset_mock()
+
+        line_follower.set_direction(Line_Follower_Direction.LEFT)
+        line_follower.update([0, 1, 1, 1])
+        new_location = (self.location[0] - 1, self.location[1] + 2)
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, Line_Follower_Direction.LEFT)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        mock_callback.assert_has_calls([
+            call("intersection", new_location)
+        ])
+        mock_callback.reset_mock()
+
+        line_follower.set_direction(Line_Follower_Direction.RIGHT)
+        line_follower.update([0, 1, 1, 1])
+        new_location = (self.location[0], self.location[1] + 2)
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, Line_Follower_Direction.RIGHT)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        mock_callback.assert_has_calls([
+            call("intersection", new_location)
+        ])
+        mock_callback.reset_mock()
+
+        # It should do nothing when the vehicle is not on a line and no other
+        # lines are detected.
+        line_follower.update([0, 0, 0, 0])
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, Line_Follower_Direction.RIGHT)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        self.assertFalse(mock_callback.called)
+        mock_callback.reset_mock()
+
+        # It should do nothing when the vehicle is not on a line and both other
+        # lines are detected.
+        line_follower.update([1, 0, 0, 1])
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, Line_Follower_Direction.RIGHT)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        self.assertFalse(mock_callback.called)
+        mock_callback.reset_mock()
+
+        # It should let the controller know when the vehicle diverged from the grid.
+        line_follower.update([1, 0, 0, 0])
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, Line_Follower_Direction.RIGHT)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        mock_callback.assert_has_calls([
+            call("diverged", "left")
+        ])
+        mock_callback.reset_mock()
+
+        line_follower.update([0, 0, 0, 1])
+        self.assertEqual(line_follower._location, new_location)
+        self.assertEqual(line_follower._direction, Line_Follower_Direction.RIGHT)
+        self.assertEqual(line_follower._state, Line_Follower_State.AT_INTERSECTION)
+        mock_callback.assert_has_calls([
+            call("diverged", "right")
+        ])
+        mock_callback.reset_mock()
+
+    def test_set_location(self):
+        from ..location.Line_Follower import Line_Follower, Line_Follower_Direction
+
+        mock_callback = MagicMock()
+        line_follower = Line_Follower(self.location, self.direction, mock_callback, self.settings)
+
+        # Direction must be one of the defined types.
+        with self.assertRaises(ValueError):
+            line_follower.set_direction("up")
+
+        # A valid direction must be set.
+        line_follower.set_direction(Line_Follower_Direction.LEFT)
+        self.assertEqual(line_follower._direction, Line_Follower_Direction.LEFT)
