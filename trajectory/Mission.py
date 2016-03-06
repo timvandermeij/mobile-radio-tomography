bbf10758baf1d9f7dc8fa366d6126340490df155
Leon Helwerda <leon.helwerda@planet.nl>
2016-03-06 19:24:11 +0100

    Mission_Cycle: Fix grid coordinate waypoints
    - Cast the space size to an integer before using it in xrange to silence
      errors about wrong types.
    - Make ranges that decrease the indices end at 0 instead of 1 so that we
      use the whole grid correctly.

--- trajectory/Mission.py
+++ trajectory/Mission.py
@@ -695,67 +695,69 @@ class Mission_Cycle(Mission_Guided):
         self.current_waypoint = None
 
         wpzip = itertools.izip_longest
-        size = self.size - 1
+        grid_size = int(self.size)
+        # Last coordinate index of the grid in both directions
+        size = grid_size - 1
 
         location = self.vehicle.location
         if location.north == 0 and location.east == 0:
             self.id = 0
             self.waypoints = itertools.chain(
                 # 0
-                wpzip(xrange(1, self.size), [], fillvalue=0),
+                wpzip(xrange(1, grid_size), [], fillvalue=0),
                 # 1
                 itertools.chain(
-                    wpzip(xrange(size - 1, 0, -1), [], fillvalue=0),
-                    wpzip([], xrange(1, self.size), fillvalue=0)
+                    wpzip(xrange(size - 1, -1, -1), [], fillvalue=0),
+                    wpzip([], xrange(1, grid_size), fillvalue=0)
                 ),
                 # 2
-                wpzip([], xrange(size - 1, 0, -1), fillvalue=0),
+                wpzip([], xrange(size - 1, -1, -1), fillvalue=0),
                 # 3
                 itertools.chain(
-                    wpzip([], xrange(1, self.size), fillvalue=0),
-                    wpzip(xrange(1, self.size), [], fillvalue=size)
+                    wpzip([], xrange(1, grid_size), fillvalue=0),
+                    wpzip(xrange(1, grid_size), [], fillvalue=size)
                 ),
                 # 4
-                wpzip(xrange(size - 1, 0, -1), [], fillvalue=size),
+                wpzip(xrange(size - 1, -1, -1), [], fillvalue=size),
                 # 5
                 itertools.chain(
-                    wpzip(xrange(1, self.size), [], fillvalue=size),
-                    wpzip([], xrange(size - 1, 0, -1), fillvalue=size)
+                    wpzip(xrange(1, grid_size), [], fillvalue=size),
+                    wpzip([], xrange(size - 1, -1, -1), fillvalue=size)
                 ),
                 # 6
-                wpzip([], xrange(1, self.size), fillvalue=size),
+                wpzip([], xrange(1, grid_size), fillvalue=size),
                 # 7
                 itertools.chain(
-                    wpzip([], xrange(size - 1, 0, -1), fillvalue=size),
-                    wpzip(xrange(size - 1, 0, -1), [], fillvalue=0)
+                    wpzip([], xrange(size - 1, -1, -1), fillvalue=size),
+                    wpzip(xrange(size - 1, -1, -1), [], fillvalue=0)
                 )
             )
         elif location.north == 0 and location.east == size:
             self.id = 1
             self.waypoints = itertools.chain(
                 # 0
-                wpzip(xrange(1, self.size), [], fillvalue=size),
+                wpzip(xrange(1, grid_size), [], fillvalue=size),
                 # 1
                 wpzip(
                     itertools.repeat(size, size * 2),
                     itertools.repeat(size, size * 2)
                 ),
                 # 2
-                wpzip([], xrange(size - 1, 0, -1), fillvalue=size),
+                wpzip([], xrange(size - 1, -1, -1), fillvalue=size),
                 # 3
                 wpzip(
                     itertools.repeat(size, size * 2),
                     itertools.repeat(0, size * 2)
                 ),
                 # 4
-                wpzip(xrange(size - 1, 0, -1), fillvalue=size),
+                wpzip(xrange(size - 1, -1, -1), fillvalue=size),
                 # 5
                 wpzip(
                     itertools.repeat(0, size * 2),
                     itertools.repeat(0, size * 2)
                 ),
                 # 6
-                wpzip(xrange([], xrange(1, self.size), fillvalue=0)),
+                wpzip(xrange([], xrange(1, grid_size), fillvalue=0)),
                 # 7
                 wpzip(
                     itertools.repeat(0, size * 2),
