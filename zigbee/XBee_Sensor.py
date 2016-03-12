9260a075c67673541c98c8e3e6cbfba7f8971e41
Leon Helwerda <leon.helwerda@planet.nl>
2016-03-13 00:32:53 +0100

    XBee_Sensor: Consolidate some common code
    Move code related to the receive callback, the location callback and the
    XBee_Packets created/handled via them from the physical and simulator
    specializations to the XBee_Sensor base class. This allows us to extend
    these packets with more fields without having to change the packets in
    too many places.
    
    Also make the location/receive callback checks more specific (must be
    callable, specify which argument is problematic) and some small
    documentation tweaks.

--- zigbee/XBee_Sensor.py
+++ zigbee/XBee_Sensor.py
@@ -1,14 +1,80 @@
+import time
 from ..core.Threadable import Threadable
+from XBee_Packet import XBee_Packet
 
 class XBee_Sensor(Threadable):
-    def __init__(self, thread_manager):
+    def __init__(self, thread_manager, location_callback, receive_callback):
         super(XBee_Sensor, self).__init__("xbee_sensor", thread_manager)
 
+        if not hasattr(location_callback, "__call__"):
+            raise TypeError("Location callback is not callable")
+
+        if not hasattr(receive_callback, "__call__"):
+            raise TypeError("Receive callback is not callable")
+
+        self._location_callback = location_callback
+        self._receive_callback = receive_callback
+
     def enqueue(self, packet, to=None):
-        raise NotImplementedError("Subclasses must implement enqueue(packet, to=None)")
+        raise NotImplementedError("Subclasses must implement `enqueue(packet, to=None)`")
 
     def _send(self):
-        raise NotImplementedError("Subclasses must implement _send()")
+        raise NotImplementedError("Subclasses must implement `_send()`")
 
     def _receive(self, packet):
-        raise NotImplementedError("Subclasses must implement _receive(packet)")
+        raise NotImplementedError("Subclasses must implement `_receive(packet)`")
+
+    def check_receive(self, packet):
+        """
+        Check whether the given `packet` should be given to the receive callback
+        rather than handling it internally.
+
+        The return value is `False` if the packet should be handled internally,
+        otherwise it is `True` and the calling method should stop handling it.
+        """
+
+        if not packet.is_private():
+            self._receive_callback(packet)
+            return True
+
+        return False
+
+    def make_rssi_broadcast_packet(self):
+        """
+        Create an XBee_Packet object containing current location data.
+
+        The resulting packet is only missing the XBee ID of the current XBee.
+        """
+
+        location = self._location_callback()
+        packet = XBee_Packet()
+        packet.set("specification", "rssi_broadcast")
+        packet.set("latitude", location[0])
+        packet.set("longitude", location[1])
+        packet.set("timestamp", time.time())
+
+        return packet
+
+    def make_ground_station_packet(self, rssi_packet):
+        """
+        Create an XBee_Packet object containing location data of the current
+        XBee and data from an XBee_Packet `rssi_packet`. The `rssi_packet`
+        must have an "rssi_broadcast" specification.
+
+        The resulting packet is only missing RSSI information.
+
+        The packet can then be sent to the ground station as an indication of
+        the signal strength between the XBee that sended the `rssi_packet` and
+        the current XBee.
+        """
+
+        location = self._location_callback()
+        ground_station_packet = XBee_Packet()
+        ground_station_packet.set("specification", "rssi_ground_station")
+        ground_station_packet.set("from_latitude", rssi_packet.get("latitude"))
+        ground_station_packet.set("from_longitude", rssi_packet.get("longitude"))
+        ground_station_packet.set("to_latitude", location[0])
+        ground_station_packet.set("to_longitude", location[1])
+
+        return ground_station_packet
+
--- zigbee/XBee_Sensor_Physical.py
+++ zigbee/XBee_Sensor_Physical.py
@@ -18,20 +18,15 @@ class XBee_Sensor_Physical(XBee_Sensor):
         Initialize the sensor.
         """
 
-        super(XBee_Sensor_Physical, self).__init__(thread_manager)
+        super(XBee_Sensor_Physical, self).__init__(thread_manager, location_callback, receive_callback)
 
         if isinstance(arguments, Arguments):
             self.settings = arguments.get_settings("xbee_sensor_physical")
         else:
             raise ValueError("'settings' must be an instance of Arguments")
 
-        if location_callback == None or receive_callback == None:
-            raise TypeError("Missing required location and receive callbacks")
-
         self.id = 0
         self.scheduler = XBee_TDMA_Scheduler(self.id, arguments)
-        self._location_callback = location_callback
-        self._receive_callback = receive_callback
         self._next_timestamp = 0
         self._serial_connection = None
         self._node_identifier_set = False
@@ -206,13 +201,8 @@ class XBee_Sensor_Physical(XBee_Sensor):
         """
 
         # Create and send the RSSI broadcast packets.
-        location = self._location_callback()
-        packet = XBee_Packet()
-        packet.set("specification", "rssi_broadcast")
-        packet.set("latitude", location[0])
-        packet.set("longitude", location[1])
+        packet = self.make_rssi_broadcast_packet()
         packet.set("sensor_id", self.id)
-        packet.set("timestamp", time.time())
 
         for index in xrange(1, self._number_of_sensors + 1):
             if index == self.id:
@@ -258,8 +248,7 @@ class XBee_Sensor_Physical(XBee_Sensor):
             packet = XBee_Packet()
             packet.unserialize(raw_packet["rf_data"])
 
-            if not packet.is_private():
-                self._receive_callback(packet)
+            if self.check_receive(packet):
                 return
 
             if packet.get("specification") == "ntp":
@@ -282,14 +271,8 @@ class XBee_Sensor_Physical(XBee_Sensor):
             # Synchronize the scheduler using the timestamp in the packet.
             self._next_timestamp = self.scheduler.synchronize(packet)
 
-            # Sanitize and complete the packet for the ground station.
-            location = self._location_callback()
-            ground_station_packet = XBee_Packet()
-            ground_station_packet.set("specification", "rssi_ground_station")
-            ground_station_packet.set("from_latitude", packet.get("latitude"))
-            ground_station_packet.set("from_longitude", packet.get("longitude"))
-            ground_station_packet.set("to_latitude", location[0])
-            ground_station_packet.set("to_longitude", location[1])
+            # Create the packet for the ground station.
+            ground_station_packet = self.make_ground_station_packet(packet)
 
             # Generate a frame ID to be able to match this packet and the
             # associated RSSI (DB command) request.
