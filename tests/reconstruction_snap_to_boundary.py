00a0dcfcf56b5bb57fc59f7b2b6b3bbe33033f92
Leon Helwerda <leon.helwerda@planet.nl>
2016-03-16 15:55:36 +0100

    Snap_To_Boundary: Add test for same points

--- tests/reconstruction_snap_to_boundary.py
+++ tests/reconstruction_snap_to_boundary.py
@@ -9,34 +9,38 @@ class TestReconstructionSnapToBoundary(unittest.TestCase):
         height = 4
         self.snapper = Snap_To_Boundary(origin, width, height)
 
+    def test_same_point(self):
+        # Equal start and end points are rejected.
+        self.assertIsNone(self.snapper.execute([2, 6], [2, 6]))
+
     def test_is_outside(self):
-        # Start point inside the network should be rejected.
-        self.assertEqual(self.snapper.execute([1, 3], [5, 3]), None)
+        # Start point inside the network are rejected.
+        self.assertIsNone(self.snapper.execute([1, 3], [5, 3]))
 
-        # End point inside the network should be rejected.
-        self.assertEqual(self.snapper.execute([-1, 5], [3, 3]), None)
+        # End point inside the network are rejected.
+        self.assertIsNone(self.snapper.execute([-1, 5], [3, 3]))
 
-        # Both start and end point inside the network should be rejected.
-        self.assertEqual(self.snapper.execute([1, 3], [3, 3]), None)
+        # Both start and end point inside the network are rejected.
+        self.assertIsNone(self.snapper.execute([1, 3], [3, 3]))
 
-        # Both start and end point outside the network should be fine.
-        self.assertNotEqual(self.snapper.execute([-1, 5], [5, 3]), None)
+        # Both start and end point outside the network are fine.
+        self.assertIsNotNone(self.snapper.execute([-1, 5], [5, 3]))
 
     def test_is_intersecting(self):
-        # Lines that do not intersect any boundary should be rejected.
-        self.assertEqual(self.snapper.execute([2, 0], [5, 2]), None)
+        # Lines that do not intersect any boundary are rejected.
+        self.assertIsNone(self.snapper.execute([2, 0], [5, 2]))
 
-        # Lines that intersect at least one boundary should be fine.
-        self.assertNotEqual(self.snapper.execute([2, 1], [5, 3]), None)
+        # Lines that intersect at least one boundary are fine.
+        self.assertIsNotNone(self.snapper.execute([2, 1], [5, 3]))
 
-        # Horizontal and vertical lines should not cause a division by zero error.
-        self.assertEqual(self.snapper.execute([-1, 7], [5, 7]), None)
-        self.assertEqual(self.snapper.execute([5, 1], [5, 7]), None)
+        # Horizontal and vertical lines do not cause a division by zero error.
+        self.assertIsNone(self.snapper.execute([-1, 7], [5, 7]))
+        self.assertIsNone(self.snapper.execute([5, 1], [5, 7]))
 
     def test_is_on_boundary(self):
         Point = namedtuple('Point', 'x y')
 
-        # Points that are already on a boundary should not be snapped.
+        # Points that are already on a boundary remain the same.
         expected = [Point(2, 6), Point(2, 2)]
         self.assertEqual(self.snapper.execute([2, 6], [2, 2]), expected)
 
@@ -62,10 +66,10 @@ class TestReconstructionSnapToBoundary(unittest.TestCase):
         expected = [Point(17/6.0, 2), Point(13/6.0, 6)]
         self.assertEqual(self.snapper.execute([3, 1], [2, 7]), expected)
 
-        # Horizontal lines should be handled properly.
+        # Horizontal lines are handled properly.
         expected = [Point(0, 4), Point(4, 4)]
         self.assertEqual(self.snapper.execute([-1, 4], [5, 4]), expected)
 
-        # Vertical lines should be handled properly.
+        # Vertical lines are handled properly.
         expected = [Point(2, 2), Point(2, 6)]
         self.assertEqual(self.snapper.execute([2, 1], [2, 7]), expected)
