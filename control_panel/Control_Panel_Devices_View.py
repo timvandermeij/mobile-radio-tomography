9b600945416cc06206bef1fc479e64cbff23fa67
Tim van der Meij <timvandermeij@gmail.com>
2016-03-20 19:52:43 +0100

    Implement device data storage and dynamic tree widget filling

--- control_panel/Control_Panel_Devices_View.py
+++ control_panel/Control_Panel_Devices_View.py
@@ -1,13 +1,17 @@
 from PyQt4 import QtGui
 from Control_Panel_View import Control_Panel_View
 
-class XBee_Device_Type(object):
+class XBee_Device_Category(object):
     COORDINATOR = 0
     END_DEVICE = 2
 
-class XBee_Device_Name(object):
-    COORDINATOR = "Coordinator"
-    END_DEVICE = "End device"
+class XBee_Device(object):
+    def __init__(self, name, id, category, address, joined):
+        self.name = name
+        self.id = id
+        self.category = category
+        self.address = address
+        self.joined = joined
 
 class Control_Panel_Devices_View(Control_Panel_View):
     def show(self):
@@ -18,22 +22,20 @@ class Control_Panel_Devices_View(Control_Panel_View):
         self._add_menu_bar()
 
         # Create the tree view.
-        tree_view = QtGui.QTreeWidget()
+        self._tree_view = QtGui.QTreeWidget()
 
         # Create the header for the tree view.
         header = QtGui.QTreeWidgetItem(["Device", "Property value"])
-        tree_view.setHeaderItem(header)
-        tree_view.header().setResizeMode(0, QtGui.QHeaderView.Stretch);
+        self._tree_view.setHeaderItem(header)
+        self._tree_view.header().setResizeMode(0, QtGui.QHeaderView.Stretch);
 
-        # Create the items for the tree view.
-        ground_station = QtGui.QTreeWidgetItem(tree_view, ["Ground station"])
-        ground_station_id = QtGui.QTreeWidgetItem(ground_station, ["ID", "0"])
-        ground_station_type = QtGui.QTreeWidgetItem(ground_station, ["Type", XBee_Device_Name.COORDINATOR])
-        ground_station_address = QtGui.QTreeWidgetItem(ground_station, ["Address", "01:55:A2:A3:55:7E"])
-        ground_station_joined = QtGui.QTreeWidgetItem(ground_station, ["Joined", "Yes"])
-
-        # Expand all items in the tree view.
-        tree_view.expandToDepth(0)
+        # Fill the tree view with the devices.
+        self._devices = [
+            XBee_Device("Ground station", 0, XBee_Device_Category.COORDINATOR, "12:34:56:78:9A:BC", True),
+            XBee_Device("Vehicle 1", 1, XBee_Device_Category.END_DEVICE, "DE:F1:23:45:67:89", False),
+            XBee_Device("Vehicle 2", 2, XBee_Device_Category.END_DEVICE, "AB:CD:EF:12:34:56", False)
+        ]
+        self._fill()
 
         # Create the refresh button.
         refresh_button = QtGui.QPushButton("Refresh")
@@ -46,7 +48,28 @@ class Control_Panel_Devices_View(Control_Panel_View):
 
         vbox = QtGui.QVBoxLayout(self._controller.central_widget)
         vbox.addLayout(hbox)
-        vbox.addWidget(tree_view)
+        vbox.addWidget(self._tree_view)
+
+    def _fill(self):
+        """
+        Fill the tree view with the device information.
+        """
+
+        categories = {
+            XBee_Device_Category.COORDINATOR: "Coordinator",
+            XBee_Device_Category.END_DEVICE: "End device"
+        }
+
+        # Add an entry in the tree view for each device.
+        for device in self._devices:
+            item = QtGui.QTreeWidgetItem(self._tree_view, [device.name])
+            item_id = QtGui.QTreeWidgetItem(item, ["ID", str(device.id)])
+            item_category = QtGui.QTreeWidgetItem(item, ["Category", categories[device.category]])
+            item_address = QtGui.QTreeWidgetItem(item, ["Address", device.address])
+            item_joined = QtGui.QTreeWidgetItem(item, ["Joined", "Yes" if device.joined else "No"])
+
+        # Expand all items in the tree view.
+        self._tree_view.expandToDepth(0)
 
     def _refresh(self):
         """
@@ -55,4 +78,5 @@ class Control_Panel_Devices_View(Control_Panel_View):
 
         # TODO: update ground station status
         # TODO: update vehicle status using node discovery
-        pass
+        self._tree_view.clear()
+        self._fill()
