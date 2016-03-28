5e26f5d6fe31e7f2e685a5dee50bf50ba6fc8a33
Tim van der Meij <timvandermeij@gmail.com>
2016-03-28 21:43:49 +0200

    Minor space fix
    Somehow this was really bugging me...

--- trajectory/Mission.py
+++ trajectory/Mission.py
@@ -14,7 +14,7 @@ from ..zigbee.XBee_Packet import XBee_Packet
 
 # We only list usable missions here, not base classes.
 __all__ = [
-    "Mission_Browse", "Mission_Cycle","Mission_Forward",
+    "Mission_Browse", "Mission_Cycle", "Mission_Forward",
     "Mission_Infrared", "Mission_Infrared_Grid", "Mission_Pathfind",
     "Mission_Search", "Mission_Square", "Mission_XBee"
 ]
