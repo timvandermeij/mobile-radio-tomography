b6d19bea6ac4fc022456e163b3f43c255c32ba6e
Leon Helwerda <leon.helwerda@planet.nl>
2016-03-28 23:32:58 +0200

    Loading view: Clean up progress display
    After inserting the ground station XBee, the loading view hangs a long
    time on "Activating ground station XBee and network...". Also, when an
    XBee is already inserted, no window is shown until the devices view is
    shown. Both of these can take an awkwardly long time.
    
    As it appears, the setup method can take quite some time. Therefore,
    turn the indeterminate progress bar (which at that point would have
    frozen because we do not update the UI often enough) into one with
    multiple steps, indicating we have progress. Make identification and
    activation two steps, and correct labels/comments for that.
    
    The window not appearing is caused by the insertion loop immediately
    taking control of the event loop, thus never updating the UI. Instead,
    make the first call to the loop also start from a (short enough) timer,
    thus freeing the UI to draw its window.
    
    Note that for simulated XBees, this causes a flash of a progressing bar,
    and for physical XBees the activation step is so fast that it is hard to
    read the label. This should not be that much of a concern since being
    fast is better than appearing to be unresponsive.

--- control_panel/Control_Panel_Loading_View.py
+++ control_panel/Control_Panel_Loading_View.py
@@ -10,9 +10,8 @@ class Control_Panel_Loading_View(Control_Panel_View):
         """
 
         # Create an indeterminate progress bar.
-        progressBar = QtGui.QProgressBar()
-        progressBar.setMinimum(0)
-        progressBar.setMaximum(0)
+        self._progressBar = QtGui.QProgressBar()
+        self._progressBar.setRange(0, 0)
 
         # Create a label.
         self._label = QtGui.QLabel("Waiting for insertion of ground station XBee...")
@@ -20,13 +19,13 @@ class Control_Panel_Loading_View(Control_Panel_View):
         # Create the layout and add the widgets.
         vbox = QtGui.QVBoxLayout(self._controller.central_widget)
         vbox.addStretch(1)
-        vbox.addWidget(progressBar)
+        vbox.addWidget(self._progressBar)
         vbox.addWidget(self._label)
         vbox.addStretch(1)
 
         # Wait for insertion of the ground station XBee.
         self._xbee_insertion_delay = self._settings.get("loading_xbee_insertion_delay") * 1000
-        self._insertion_loop()
+        QtCore.QTimer.singleShot(self._xbee_insertion_delay, lambda: self._insertion_loop())
 
     def _insertion_loop(self):
         """
@@ -38,9 +37,11 @@ class Control_Panel_Loading_View(Control_Panel_View):
                 self._controller.usb_manager.index()
                 self._controller.usb_manager.get_xbee_device()
 
-            # We now know that a valid XBee device has been inserted.
-            # Therefore update the label and proceed with activating the XBee.
-            self._label.setText("Activating ground station XBee and wireless network...")
+            # An XBee has been inserted, but we need to check that it actually 
+            # belongs to a ground station XBee.
+            self._progressBar.setRange(0, 2)
+            self._progressBar.setValue(1)
+            self._label.setText("Identifying the inserted XBee...")
             self._label.repaint()
             self._controller.app.processEvents()
 
@@ -50,6 +51,13 @@ class Control_Panel_Loading_View(Control_Panel_View):
                                            "The inserted XBee device is not a ground station.")
                 self._controller.window.close()
                 sys.exit(1)
+
+            # We now know that a valid XBee device has been inserted.
+            # Therefore update the label and proceed with activating the XBee.
+            self._progressBar.setValue(2)
+            self._label.setText("Activating ground station XBee and wireless network...")
+            self._label.repaint()
+            self._controller.app.processEvents()
 
             self._controller.xbee.activate()
             self._controller.show_view(Control_Panel_View_Name.DEVICES)
