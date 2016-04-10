timvandermeij/mobile-radio-tomography

commit fe3551d5332d12e0f41a42210720332d370c6294

Mission: Add wait commands in XBee mission, clean up waypoints
Make a base Mission_Auto method for adding waypoints to the vehicle's
mission commands. This method now handles all the conversions needed for
locations in specific geometries, and adds the wait command if enabled.

Use this method for adding waypoints in Mission_XBee, so that the wait
commands are added when XBee waypoint synchronization is enabled. Also
track the received waypoint count ourselves instead of calculating the
next index in a fragile and vehicle-dependent manner.

trajectory/Mission.py
@@ -298,12 +298,41 @@ class Mission_Auto(Mission):
         raise NotImplementedError("Must be implemented in child class")
 
     def add_takeoff(self):
-        # Add takeoff command. This is ignored if the vehicle is already in the 
-        # air, or if the vehicle is a ground vehicle.
+        """
+        Add takeoff command. The command is ignored if the vehicle is already
+        in the air, or if the vehicle is a ground vehicle. In this case, the
+        command may even be not added at all, and the altitude is set to zero.
+        """
+
         has_takeoff = self.vehicle.add_takeoff(self.altitude)
         if not has_takeoff:
             self.altitude = 0.0
             self._first_waypoint = 0
+
+    def add_waypoint(self, point):
+        """
+        Add a waypoint location object `point` to the vehicle's mission command
+        waypoints.
+
+        If XBee synchronization is enabled, also adds a wait command afterward.
+        """
+
+        # Handle local locations, points without a specific altitude and 
+        # non-spherical geometries.
+        if isinstance(point, LocationLocal):
+            down = point.down if point.down != 0.0 else -self.altitude
+            point = LocationLocal(point.north, point.east, down)
+        else:
+            alt = point.alt if point.alt != 0.0 else self.altitude
+            if isinstance(self.geometry, Geometry_Spherical):
+                point = LocationGlobalRelative(point.lat, point.lon, alt)
+            else:
+                point = LocationLocal(point.lat, point.lon, -alt)
+
+        self.vehicle.add_waypoint(point)
+
+        if self._xbee_synchronization:
+            self.vehicle.add_wait()
 
     def add_commands(self):
         """
@@ -318,16 +347,7 @@ class Mission_Auto(Mission):
         # Add the waypoint commands.
         points = self.get_waypoints()
         for point in points:
-            # Handle non-spherical geometries
-            if isinstance(point, LocationLocal):
-                point = LocationLocal(point.north, point.east, -self.altitude)
-            else:
-                point = LocationGlobalRelative(point.lat, point.lon, self.altitude)
-
-            self.vehicle.add_waypoint(point)
-
-            if self._xbee_synchronization:
-                self.vehicle.add_wait()
+            self.add_waypoint(point)
 
         # Send commands to vehicle and update.
         self.vehicle.update_mission()
@@ -905,6 +925,7 @@ class Mission_XBee(Mission_Auto):
         self.environment.add_packet_action("waypoint_done", self._complete_waypoints)
 
         self._waypoints_complete = False
+        self._next_index = 0
 
     def arm_and_takeoff(self):
         # Wait until all the waypoints have been received before arming.
@@ -930,11 +951,6 @@ class Mission_XBee(Mission_Auto):
         # Commands are added when they arrive, not in here.
         pass
 
-    def _get_next_index(self):
-        # Number of waypoint commands in the vehicle for each waypoint.
-        commands = 2 if self._xbee_synchronization else 1
-        return self.vehicle.count_waypoints() / commands - self._first_waypoint
-
     def _send_ack(self):
         """
         Send a "waypoint_ack" packet to the ground station.
@@ -948,7 +964,7 @@ class Mission_XBee(Mission_Auto):
 
         ack_packet = XBee_Packet()
         ack_packet.set("specification", "waypoint_ack")
-        ack_packet.set("next_index", self._get_next_index())
+        ack_packet.set("next_index", self._next_index)
         ack_packet.set("sensor_id", xbee_sensor.id)
 
         xbee_sensor.enqueue(ack_packet, to=0)
@@ -983,7 +999,7 @@ class Mission_XBee(Mission_Auto):
             return
 
         index = packet.get("index")
-        if index != self._get_next_index():
+        if index != self._next_index:
             # Send a reply saying what index were are currently at and ignore 
             # the packet, which may be duplicate or out of order.
             self._send_ack()
@@ -991,10 +1007,10 @@ class Mission_XBee(Mission_Auto):
 
         latitude = packet.get("latitude")
         longitude = packet.get("longitude")
-        if isinstance(self.geometry, Geometry_Spherical):
-            point = LocationGlobalRelative(latitude, longitude, self.altitude)
-        else:
-            point = LocationLocal(latitude, longitude, -self.altitude)
 
-        self.vehicle.add_waypoint(point)
+        # Make a location waypoint. `add_waypoint` handles any further 
+        # conversion steps.
+        point = LocationGlobalRelative(latitude, longitude, 0.0)
+        self.add_waypoint(point)
+        self._next_index += 1
         self._send_ack()

commit 3997f55c4c6de80f47eb0284461f040553d5a043

Mission_XBee: Add tests
Create a test that ensures correct behavior from the XBee mission.
Included are tests for the setup method and for the packet callbacks of
clearing waypoints, adding a waypoint and done completing the waypoints.
The latter also tests whether XBee waypoint synchronization works
correctly, i.e., the vehicle waits at a waypoint with a wait command
until the location packet is valid.

The test uncovered a problem in an edge case that was introduced in
fe3551d5332d12e0f41a42210720332d370c6294: when clearing a non-empty
mission, the next index tracking the waypoint count was not reset.

trajectory/Mission.py
@@ -982,6 +982,7 @@ class Mission_XBee(Mission_Auto):
         self.clear_mission()
         # Add a takeoff command for flying vehicles that use it.
         self.add_takeoff()
+        self._next_index = 0
         self._send_ack()
 
     def _add_waypoint(self, packet):

tests/mission_xbee.py
@@ -1,0 +1,171 @@
+from mock import patch
+from dronekit import LocationLocal
+from ..trajectory.Mission import Mission_XBee
+from ..vehicle.Robot_Vehicle import Robot_State
+from ..zigbee.XBee_Sensor_Simulator import XBee_Sensor_Simulator
+from ..zigbee.XBee_Packet import XBee_Packet
+from environment import EnvironmentTestCase
+
+class TestMissionXBee(EnvironmentTestCase):
+    def setUp(self):
+        self.register_arguments([
+            "--vehicle-class", "Robot_Vehicle_Arduino",
+            "--geometry-class", "Geometry", "--space-size", "10",
+            "--xbee-synchronization", "--xbee-type", "simulator",
+            "--closeness", "0"
+        ], use_infrared_sensor=False)
+
+        super(TestMissionXBee, self).setUp()
+
+        self.vehicle = self.environment.get_vehicle()
+
+        settings = self.arguments.get_settings("mission")
+        self.mission = Mission_XBee(self.environment, settings)
+        self.xbee = self.environment.get_xbee_sensor()
+
+    def test_setup(self):
+        with patch('sys.stdout'):
+            self.mission.setup()
+
+        packet_actions = self.environment._packet_callbacks.keys()
+        self.assertIn("waypoint_clear", packet_actions)
+        self.assertIn("waypoint_add", packet_actions)
+        self.assertIn("waypoint_done", packet_actions)
+        self.assertFalse(self.mission._waypoints_complete)
+        self.assertEqual(self.mission._next_index, 0)
+
+    def _send_waypoint_add(self, index, latitude, longitude):
+        packet = XBee_Packet()
+        packet.set("specification", "waypoint_add")
+        packet.set("index", index)
+        packet.set("latitude", latitude)
+        packet.set("longitude", longitude)
+        packet.set("to_id", self.xbee.id)
+
+        with patch('sys.stdout'):
+            self.environment.receive_packet(packet)
+
+    @patch.object(XBee_Sensor_Simulator, "enqueue")
+    def test_clear(self, enqueue_mock):
+        with patch('sys.stdout'):
+            self.mission.setup()
+
+        self._send_waypoint_add(0, 4.0, 2.0)
+        enqueue_mock.reset_mock()
+
+        packet = XBee_Packet()
+        packet.set("specification", "waypoint_clear")
+        packet.set("to_id", self.xbee.id)
+
+        with patch('sys.stdout'):
+            self.environment.receive_packet(packet)
+
+        self.assertEqual(enqueue_mock.call_count, 1)
+        args, kwargs = enqueue_mock.call_args
+        self.assertEqual(len(args), 1)
+        self.assertIsInstance(args[0], XBee_Packet)
+        self.assertEqual(args[0].get_all(), {
+            "specification": "waypoint_ack",
+            "next_index": 0,
+            "sensor_id": self.xbee.id
+        })
+        self.assertEqual(kwargs, {"to": 0})
+
+        self.assertEqual(self.vehicle._waypoints, [])
+
+    @patch.object(XBee_Sensor_Simulator, "enqueue")
+    def test_add(self, enqueue_mock):
+        with patch('sys.stdout'):
+            self.mission.setup()
+
+        self._send_waypoint_add(0, 1.0, 4.0)
+
+        self.assertEqual(enqueue_mock.call_count, 1)
+        args, kwargs = enqueue_mock.call_args
+        self.assertEqual(len(args), 1)
+        self.assertIsInstance(args[0], XBee_Packet)
+        self.assertEqual(args[0].get_all(), {
+            "specification": "waypoint_ack",
+            "next_index": 1,
+            "sensor_id": self.xbee.id
+        })
+        self.assertEqual(kwargs, {"to": 0})
+
+        self.assertEqual(self.vehicle._waypoints, [(1, 4), None])
+
+    @patch.object(XBee_Sensor_Simulator, "enqueue")
+    def test_add_wrong_index(self, enqueue_mock):
+        with patch('sys.stdout'):
+            self.mission.setup()
+
+        self._send_waypoint_add(42, 3.0, 2.0)
+
+        self.assertEqual(enqueue_mock.call_count, 1)
+        args, kwargs = enqueue_mock.call_args
+        self.assertEqual(len(args), 1)
+        self.assertIsInstance(args[0], XBee_Packet)
+        self.assertEqual(args[0].get_all(), {
+            "specification": "waypoint_ack",
+            "next_index": 0,
+            "sensor_id": self.xbee.id
+        })
+        self.assertEqual(kwargs, {"to": 0})
+
+        self.assertEqual(self.vehicle._waypoints, [])
+
+    @patch.object(XBee_Sensor_Simulator, "enqueue")
+    def test_done(self, enqueue_mock):
+        with patch('sys.stdout'):
+            self.mission.setup()
+
+        self._send_waypoint_add(0, 1.0, 0.0)
+        self._send_waypoint_add(1, 2.0, 0.0)
+        self._send_waypoint_add(2, 3.0, 0.0)
+        self._send_waypoint_add(3, 4.0, 0.0)
+
+        packet = XBee_Packet()
+        packet.set("specification", "waypoint_done")
+        packet.set("to_id", self.xbee.id)
+
+        with patch('sys.stdout'):
+            self.environment.receive_packet(packet)
+
+        self.assertTrue(self.mission._waypoints_complete)
+        self.assertEqual(self.vehicle._waypoints, [
+            (1, 0), None, (2, 0), None, (3, 0), None, (4, 0), None
+        ])
+
+        with patch('sys.stdout'):
+            self.mission.arm_and_takeoff()
+            self.mission.start()
+
+        self.assertEqual(self.vehicle.mode.name, "AUTO")
+        self.assertTrue(self.vehicle.armed)
+
+        self.vehicle._check_state()
+        self.assertEqual(self.vehicle._current_waypoint, 0)
+        self.assertEqual(self.vehicle.get_waypoint(), LocationLocal(1, 0, 0))
+        self.assertFalse(self.vehicle.is_wait())
+        with patch('sys.stdout'):
+            self.assertTrue(self.mission.check_waypoint())
+
+        self.vehicle._location = (1, 0)
+        self.vehicle._state = Robot_State("intersection")
+        self.vehicle._check_state()
+        with patch('sys.stdout'):
+            self.assertTrue(self.mission.check_waypoint())
+
+        self.assertEqual(self.vehicle._current_waypoint, 1)
+        self.assertEqual(self.vehicle.get_waypoint(), None)
+        self.assertTrue(self.vehicle.is_wait())
+
+        # The mission waits for the other XBee to send a valid location packet.
+        self.assertTrue(self.environment.location_valid(other_valid=True))
+        with patch('sys.stdout'):
+            self.assertTrue(self.mission.check_waypoint())
+
+        self.assertEqual(self.vehicle.get_waypoint(), LocationLocal(2, 0, 0))
+        self.assertFalse(self.vehicle.is_wait())
+        self.vehicle._check_state()
+        self.assertEqual(self.vehicle._current_waypoint, 2)
+        self.assertEqual(self.vehicle._state.name, "move")