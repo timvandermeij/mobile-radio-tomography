0a9634e1136956808c961e8831071c066fad024a
Leon Helwerda <leon.helwerda@planet.nl>
2016-06-20 12:01:47 +0200

    Pylint: Consider user site packages as external packages too
    Otherwise they are considered local imports, which is not right.
    Also add system site packages when there are additional 64-bit site
    packages.

--- plugins/pylint_standard_module.py
+++ plugins/pylint_standard_module.py
@@ -3,6 +3,7 @@
 # packages, rather than guessing that they are part of the core modules.
 
 from functools import partial
+import site
 import astroid.modutils as modutils
 import pylint.checkers.imports as imports
 
@@ -26,3 +27,13 @@ def register(linter):
         new_is_standard_module = partial(is_standard_module, linter)
         modutils.is_standard_module = new_is_standard_module
         imports.is_standard_module = new_is_standard_module
+
+        # Add the user site packages to external import locations, if it exists
+        try:
+            for checker in linter._checkers['imports']:
+                checker._site_packages.update(site.getsitepackages())
+                checker._site_packages.add(site.getusersitepackages())
+        except AttributeError:
+            # Running in a virtual environment, in which case there is no
+            # `site.getusersitepackages()` method.
+            pass
