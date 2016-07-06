40e7e56fe1a06bccae674f00cbedc4c69e8b8076
Leon Helwerda <leon.helwerda@planet.nl>
2016-07-06 17:41:42 +0200

    Fix Geometry in Raspberry Pi start script

--- raspberry-pi-start.sh
+++ raspberry-pi-start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/alarm/mobile-radio-tomography
-stdbuf -oL -eL python2 mission_basic.py --vehicle-class Robot_Vehicle_Arduino_Full --geometry-class Geometry --servo-pins --distance-sensors --no-plot --no-viewer --speed 0.3 --rf-sensor-class RF_Sensor_Physical_Texas_Instruments --synchronize --closeness 0 > logs/output$$.log 2> logs/error$$.log
+stdbuf -oL -eL python2 mission_basic.py --vehicle-class Robot_Vehicle_Arduino_Full --geometry-class Geometry_Grid --servo-pins --distance-sensors --no-plot --no-viewer --speed 0.3 --rf-sensor-class RF_Sensor_Physical_Texas_Instruments --synchronize --closeness 0 > logs/output$$.log 2> logs/error$$.log
