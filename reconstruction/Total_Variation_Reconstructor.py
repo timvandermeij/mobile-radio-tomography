a2876245804c3ea487fc46c5a12a30b27765b360
Tim van der Meij <timvandermeij@gmail.com>
2016-07-22 00:39:12 +0200

    Reconstruction total variation: improved method names
    Be less verbose as we are already in the total variation reconstructor
    class. Replace `gradient` with `derivative` as that is what we are
    actually calculating.

--- reconstruction/Total_Variation_Reconstructor.py
+++ reconstruction/Total_Variation_Reconstructor.py
@@ -57,13 +57,13 @@ class Total_Variation_Reconstructor(Reconstructor):
         options = {
             "maxiter": self._solver_iterations
         }
-        total_variation = partial(self._calculate_total_variation, A, b)
-        total_variation_gradient = partial(self._calculate_total_variation_gradient, A, b)
+        total_variation = partial(self._calculate, A, b)
+        total_variation_derivative = partial(self._calculate_derivative, A, b)
         solution = scipy.optimize.minimize(total_variation, self._guess, options=options,
-                                           jac=total_variation_gradient, method=self._solver_method)
+                                           jac=total_variation_derivative, method=self._solver_method)
         return solution.x
 
-    def _calculate_total_variation(self, A, b, x):
+    def _calculate(self, A, b, x):
         """
         Calculate the total variation for a given solution `x`. This method
         represents what will be minimized by SciPy's optimizer.
@@ -73,10 +73,9 @@ class Total_Variation_Reconstructor(Reconstructor):
         formula used for this method.
         """
 
-        return (0.5 * np.linalg.norm((A * x) - b) +
-                self._alpha * self._calculate_total_variation_factor(x))
+        return 0.5 * np.linalg.norm((A * x) - b) + self._alpha * self._calculate_factor(x)
 
-    def _calculate_total_variation_factor(self, x):
+    def _calculate_factor(self, x):
         """
         Calculate the total variation factor for a given solution `x`. This factor
         is used in the method above to calculate the total variation of `x`.
@@ -88,12 +87,9 @@ class Total_Variation_Reconstructor(Reconstructor):
 
         return np.sum(np.sqrt(np.gradient(x) ** 2 + self._beta ** 2))
 
-    def _calculate_total_variation_gradient(self, A, b, x):
+    def _calculate_derivative(self, A, b, x):
         """
-        Calculate the total variation gradient for a given solution `x`. We take
-        the gradient, a vector of partial derivatives, to be equal to the total
-        derivative (approximately the combination of the partial derivatives)
-        and calculate that instead.
+        Calculate the total variation derivative for a given solution `x`.
         """
 
         least_squares_derivative = ((A.T * A) * x) - (A.T * b)
